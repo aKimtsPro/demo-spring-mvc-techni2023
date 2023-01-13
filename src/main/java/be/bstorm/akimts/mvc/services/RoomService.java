6b18ec29069cfb2663ab15082e60caf43af97a7b
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2023-01-13 15:26:33 +0100

    demo formulaires

--- src/main/java/be/bstorm/akimts/mvc/services/RoomService.java
+++ src/main/java/be/bstorm/akimts/mvc/services/RoomService.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 @Service
 public class RoomService {
